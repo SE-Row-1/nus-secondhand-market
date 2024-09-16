9cfe6376dbd1fc884a6dc2e3ef7f491071c204ad
Yuwang Cai <mrcaidev@gmail.com>
2024-09-16 23:31:39 +0800

    feat: add button to empty home page to test components validity

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { GithubIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <h1>NUS Second-Hand Market</h1>
+    <main className="min-h-screen grid place-items-center">
+      <Button asChild>
+        <Link
+          href="https://github.com/SE-Row-1/nus-secondhand-market"
+          target="_blank"
+        >
+          <GithubIcon className="size-4 mr-2" />
+          Source code
+        </Link>
+      </Button>
     </main>
   );
 }
