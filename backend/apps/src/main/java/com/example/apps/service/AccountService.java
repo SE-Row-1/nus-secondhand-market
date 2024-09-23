31426c6159da6c31833ca6cc899f9dbc2633d1a6
GuoHeZuYa <85046837+GuoHeZuYa@users.noreply.github.com>
2024-09-23 21:33:42 +0800

    remove backend content

--- backend/apps/src/main/java/com/example/apps/service/AccountService.java
+++
@@ -1,4 +0,0 @@
-package com.example.apps.service;
-
-
-public interface AccountService {}
--- backend/apps/src/main/java/com/example/apps/service/AccountServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.apps.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AccountServiceImpl implements AccountService{
-
-}
