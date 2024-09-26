d50e55536064da243f626f6de13d9d20c8fb5b8f
Yuwang Cai <mrcaidev@gmail.com>
2024-09-26 10:42:18 +0800

    test: add mock route

---
+++ frontend/src/app/api/auth/me/route.ts
@@ -0,0 +1,71 @@
+import type { Account } from "@/types";
+import { cookies } from "next/headers";
+import { NextResponse, type NextRequest } from "next/server";
+
+const mockAccount: Account = {
+  id: 1,
+  email: "e1351826@u.nus.edu",
+  nickname: "mrcaidev",
+  avatar_url: "https://avatars.githubusercontent.com/u/78269445?v=4",
+  department: {
+    id: 0,
+    acronym: "ISS",
+    name: "Institute of System Science",
+  },
+  phone: "6580843976",
+  preferred_currency: "CNY",
+  created_at: "2024-09-23 12:19:10.415264+00",
+  deleted_at: null,
+};
+
+export async function GET() {
+  const accessToken = cookies().get("access_token")?.value;
+
+  if (!accessToken) {
+    return NextResponse.json(
+      { error: "Please log in first." },
+      { status: 401 },
+    );
+  }
+
+  return NextResponse.json(mockAccount, { status: 200 });
+}
+
+export async function POST(request: NextRequest) {
+  const { email } = await request.json();
+
+  cookies().set({
+    name: "access_token",
+    value: process.env["TEST_USER_JWT"] ?? "",
+    path: "/",
+    domain: "localhost",
+    maxAge: 60 * 60 * 24 * 7,
+    expires: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+  });
+
+  return NextResponse.json({ ...mockAccount, email }, { status: 201 });
+}
+
+export async function PATCH(request: NextRequest) {
+  const accessToken = cookies().get("access_token")?.value;
+
+  if (!accessToken) {
+    return NextResponse.json(
+      { error: "Please log in first." },
+      { status: 401 },
+    );
+  }
+
+  const json = await request.json();
+
+  return NextResponse.json({ ...mockAccount, ...json }, { status: 200 });
+}
+
+export async function DELETE() {
+  cookies().set("access_token", "", { maxAge: 0 });
+
+  return new NextResponse(null, { status: 204 });
+}
