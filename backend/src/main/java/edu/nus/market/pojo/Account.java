1aec4cb8a5a0cce8121cddfadfb8779a075bf42f
Jyf <1254023440@qq.com>
2024-09-26 19:38:47 +0800

    fix: add annotation for account to match the format of DB

--- backend/src/main/java/edu/nus/market/pojo/Account.java
+++ backend/src/main/java/edu/nus/market/pojo/Account.java
@@ -3,21 +3,40 @@ package edu.nus.market.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Account {
+
     int id;
+
     String email;
+
     String nickname;
+
+    @Column(name = "password_hash")
     String passwordHash;
+
+    @Column(name = "password_salt")
     String passwordSalt;
+
+    @Column(name = "avatar_url")
     String avatarUrl;
+
+    @Column(name = "department_id")
     int departmentId;
+
     String phone;
+
+    @Column(name = "preferred_currency")
     String preferredCurrency;
+
+    @Column(name = "created_at")
     String createdAt;
+
+    @Column(name = "deleted_at")
     String deletedAt;
 
     public Account(Register register){
