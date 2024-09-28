9160ad120b340ab2a6885f1f0461c8601b778a80
Yuwang Cai <mrcaidev@gmail.com>
2024-09-28 16:34:08 +0800

    feat: items collection schema

--- services/item/src/utils/db.ts
+++ services/item/src/utils/db.ts
@@ -1,7 +1,8 @@
+import type { Item } from "@/types";
 import { MongoClient } from "mongodb";
 
 const client = await MongoClient.connect(process.env.MONGO_DB_URI);
 
 const db = client.db(process.env.MONGO_DB_NAME);
 
-export const itemsCollection = db.collection("items");
+export const itemsCollection = db.collection<Item>("items");
