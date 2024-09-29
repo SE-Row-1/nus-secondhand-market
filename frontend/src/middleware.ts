8409c4aecf75d15640c188010bed1608c830e381
Yuwang Cai <mrcaidev@gmail.com>
2024-09-29 21:22:50 +0800

    fix: protect /settings page

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,14 +1,27 @@
 import type { MiddlewareConfig, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-const NO_AUTH_PATHNAMES = ["/login", "/register"];
+const NO_AUTH_REG_EXPS = [/^\/login$/, /^\/register$/];
+const AUTH_REG_EXPS = [/^\/settings/];
 
 export async function middleware(request: NextRequest) {
   const accessToken = request.cookies.get("access_token");
 
-  if (accessToken && NO_AUTH_PATHNAMES.includes(request.nextUrl.pathname)) {
+  if (
+    accessToken &&
+    NO_AUTH_REG_EXPS.some((regExp) => regExp.test(request.nextUrl.pathname))
+  ) {
     const redirectUrl = request.nextUrl.clone();
     redirectUrl.pathname = "/";
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (
+    !accessToken &&
+    AUTH_REG_EXPS.some((regExp) => regExp.test(request.nextUrl.pathname))
+  ) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = "/login";
     return NextResponse.redirect(redirectUrl);
   }
 
