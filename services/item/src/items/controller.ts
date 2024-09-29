c9cd875a0b6556a1be2fd8054ad9e31b1736397c
Yuwang Cai <mrcaidev@gmail.com>
2024-09-29 13:17:21 +0800

    style: rename collection to repository

--- services/item/src/items/controller.ts
+++ services/item/src/items/controller.ts
@@ -1,9 +1,9 @@
-import { itemsCollection } from "@/utils/db";
+import { itemsRepository } from "@/utils/db";
 import { Hono } from "hono";
 
 export const itemsController = new Hono();
 
 itemsController.get("/", async (context) => {
-  const items = await itemsCollection.find().toArray();
+  const items = await itemsRepository.find().toArray();
   return context.json(items);
 });
--- services/item/src/utils/db.ts
+++ services/item/src/utils/db.ts
@@ -5,4 +5,4 @@ const client = await MongoClient.connect(process.env.MONGO_DB_URI);
 
 const db = client.db(process.env.MONGO_DB_NAME);
 
-export const itemsCollection = db.collection<Item>("items");
+export const itemsRepository = db.collection<Item>("items");
