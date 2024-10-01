b41e48d01de347b3f5a155dd482d4ea8e1c2f2dd
Yuwang Cai <mrcaidev@gmail.com>
2024-10-01 20:17:14 +0800

    feat: update email card

--- frontend/src/app/settings/cards/update-email-card.tsx
+++ frontend/src/app/settings/cards/update-email-card.tsx
@@ -10,31 +10,86 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { SaveIcon } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
+import type { Account } from "@/types";
+import { ClientRequester } from "@/utils/requester/client";
+import { Loader2Icon, SaveIcon } from "lucide-react";
+import { type FormEvent } from "react";
+import useSWRMutation from "swr/mutation";
+import * as v from "valibot";
 
-export function UpdateEmailCard() {
+const formSchema = v.object({
+  email: v.pipe(
+    v.string("Email should be a text string."),
+    v.endsWith("@u.nus.edu", "Email should be a NUS email address."),
+  ),
+});
+
+type Props = {
+  initialEmail: string;
+};
+
+export function UpdateEmailCard({ initialEmail }: Props) {
+  const { toast } = useToast();
+
+  const { trigger, isMutating } = useSWRMutation<
+    Account,
+    Error,
+    string,
+    FormEvent<HTMLFormElement>
+  >(
+    "auth/me",
+    async (_, { arg: event }) => {
+      event.preventDefault();
+
+      const formData = Object.fromEntries(new FormData(event.currentTarget));
+
+      const { email } = v.parse(formSchema, formData);
+
+      return await new ClientRequester().patch<Account>("auth/me", {
+        email,
+      });
+    },
+    {
+      populateCache: true,
+      revalidate: false,
+      throwOnError: false,
+      onError: (error) => {
+        toast({
+          variant: "destructive",
+          title: "Failed to update Email",
+          description: error.message,
+        });
+      },
+    },
+  );
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Email</CardTitle>
         <CardDescription>
-          Preferrably your EDU email address, used for identity verification and
-          login.
+          Your NUS email address, used for identity verification and login.
         </CardDescription>
       </CardHeader>
-      <form>
+      <form onSubmit={trigger}>
         <CardContent>
           <Input
             type="email"
             name="email"
             required
+            defaultValue={initialEmail ?? ""}
             placeholder="e1234567@u.nus.edu"
             id="email"
           />
         </CardContent>
         <CardFooter className="border-t px-6 py-4">
-          <Button type="submit">
-            <SaveIcon className="size-4 mr-2" />
+          <Button disabled={isMutating} type="submit">
+            {isMutating ? (
+              <Loader2Icon className="size-4 mr-2 animate-spin" />
+            ) : (
+              <SaveIcon className="size-4 mr-2" />
+            )}
             Save
           </Button>
         </CardFooter>
--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,11 +1,15 @@
+import type { Account } from "@/types";
+import { ServerRequester } from "@/utils/requester/server";
 import { DeleteAccountCard } from "./cards/delete-account-card";
 import { UpdateEmailCard } from "./cards/update-email-card";
 import { UpdatePasswordCard } from "./cards/update-password-card";
 
-export default function SettingsPage() {
+export default async function SettingsPage() {
+  const me = await new ServerRequester().get<Account>("auth/me");
+
   return (
     <div className="grid gap-6">
-      <UpdateEmailCard />
+      <UpdateEmailCard initialEmail={me.email} />
       <UpdatePasswordCard />
       <DeleteAccountCard />
     </div>
