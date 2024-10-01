927f8921416701e763f7e89456007482b1441261
Yuwang Cai <mrcaidev@gmail.com>
2024-10-01 20:12:51 +0800

    feat: update nickname

--- frontend/src/app/settings/cards/update-nickname-card.tsx
+++ frontend/src/app/settings/cards/update-nickname-card.tsx
@@ -10,9 +10,61 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { SaveIcon } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
+import type { Account } from "@/types";
+import { ClientRequester } from "@/utils/requester/client";
+import { Loader2Icon, SaveIcon } from "lucide-react";
+import { type FormEvent } from "react";
+import useSWRMutation from "swr/mutation";
+import * as v from "valibot";
 
-export function UpdateNicknameCard() {
+const formSchema = v.object({
+  nickname: v.pipe(
+    v.string("Nickname should be a text string."),
+    v.minLength(2, "Nickname should be at least 2 characters long."),
+    v.maxLength(20, "Nickname should be at most 20 characters long."),
+  ),
+});
+
+type Props = {
+  initialNickname: string | null;
+};
+
+export function UpdateNicknameCard({ initialNickname }: Props) {
+  const { toast } = useToast();
+
+  const { trigger, isMutating } = useSWRMutation<
+    Account,
+    Error,
+    string,
+    FormEvent<HTMLFormElement>
+  >(
+    "auth/me",
+    async (_, { arg: event }) => {
+      event.preventDefault();
+
+      const formData = Object.fromEntries(new FormData(event.currentTarget));
+
+      const { nickname } = v.parse(formSchema, formData);
+
+      return await new ClientRequester().patch<Account>("auth/me", {
+        nickname,
+      });
+    },
+    {
+      populateCache: true,
+      revalidate: false,
+      throwOnError: false,
+      onError: (error) => {
+        toast({
+          variant: "destructive",
+          title: "Failed to update nickname",
+          description: error.message,
+        });
+      },
+    },
+  );
+
   return (
     <Card>
       <CardHeader>
@@ -21,19 +73,24 @@ export function UpdateNicknameCard() {
           Your preferred name, displayed to everyone else on the platform.
         </CardDescription>
       </CardHeader>
-      <form>
+      <form onSubmit={trigger}>
         <CardContent>
           <Input
             type="text"
             name="nickname"
             required
+            defaultValue={initialNickname ?? ""}
             placeholder="2-20 characters"
             id="nickname"
           />
         </CardContent>
         <CardFooter className="border-t px-6 py-4">
-          <Button type="submit">
-            <SaveIcon className="size-4 mr-2" />
+          <Button disabled={isMutating} type="submit">
+            {isMutating ? (
+              <Loader2Icon className="size-4 mr-2 animate-spin" />
+            ) : (
+              <SaveIcon className="size-4 mr-2" />
+            )}
             Save
           </Button>
         </CardFooter>
--- frontend/src/app/settings/display/page.tsx
+++ frontend/src/app/settings/display/page.tsx
@@ -1,11 +1,15 @@
+import type { Account } from "@/types";
+import { ServerRequester } from "@/utils/requester/server";
 import { UpdateAvatarCard } from "../cards/update-avatar-card";
 import { UpdateNicknameCard } from "../cards/update-nickname-card";
 
-export default function DisplaySettingsPage() {
+export default async function DisplaySettingsPage() {
+  const me = await new ServerRequester().get<Account>("auth/me");
+
   return (
     <div className="grid gap-6">
       <UpdateAvatarCard />
-      <UpdateNicknameCard />
+      <UpdateNicknameCard initialNickname={me.nickname} />
     </div>
   );
 }
