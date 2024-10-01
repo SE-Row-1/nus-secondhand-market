511ff10c203ea1e1d3e10b95fd3b07c87e169c3f
Yuwang Cai <mrcaidev@gmail.com>
2024-10-01 22:00:52 +0800

    fix: typeguard server-fetched me

--- frontend/src/app/settings/contacts/page.tsx
+++ frontend/src/app/settings/contacts/page.tsx
@@ -1,15 +1,15 @@
+import type { Account } from "@/types";
 import { ServerRequester } from "@/utils/requester/server";
 import { UpdateWhatsappCard } from "../cards/update-whatsapp-card";
-import type { Account } from "@/types";
 
 export default async function ContactsSettingsPage() {
-  const me = await new ServerRequester().get<Account>("auth/me");
+  const me = await new ServerRequester().get<Account | undefined>("auth/me");
 
   return (
     <div className="grid gap-6">
       <UpdateWhatsappCard
-        initialPhoneCode={me.phone_code}
-        initialPhoneNumber={me.phone_number}
+        initialPhoneCode={me?.phone_code ?? null}
+        initialPhoneNumber={me?.phone_number ?? null}
       />
     </div>
   );
--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -4,11 +4,11 @@ import { DeleteAccountCard } from "./cards/delete-account-card";
 import { UpdateEmailCard } from "./cards/update-email-card";
 
 export default async function SettingsPage() {
-  const me = await new ServerRequester().get<Account>("auth/me");
+  const me = await new ServerRequester().get<Account | undefined>("auth/me");
 
   return (
     <div className="grid gap-6">
-      <UpdateEmailCard initialEmail={me.email} />
+      <UpdateEmailCard initialEmail={me?.email ?? ""} />
       <DeleteAccountCard />
     </div>
   );
