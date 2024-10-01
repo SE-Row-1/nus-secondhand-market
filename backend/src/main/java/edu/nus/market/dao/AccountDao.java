dabbc118c3b45b145e2cecc4e4dec4aa2275468b
Jyf <1254023440@qq.com>
2024-10-01 23:25:39 +0800

    fix: change the SQL to meet the soft delete requirement

--- backend/src/main/java/edu/nus/market/dao/AccountDao.java
+++ backend/src/main/java/edu/nus/market/dao/AccountDao.java
@@ -1,22 +1,19 @@
 package edu.nus.market.dao;
 
 import edu.nus.market.pojo.Account;
-
-import edu.nus.market.pojo.Register;
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.annotations.Select;
-
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface AccountDao {
     @Select("SELECT id, email, nickname, avatar_url, department_id, password_hash, password_salt, phone_code, phone_number, preferred_currency, " +
-        "created_at, deleted_at FROM account WHERE id = #{id}")
+        "created_at, deleted_at FROM account WHERE id = #{id} AND deleted_at IS NULL")
     Account getAccountById(int id);
 
     @Select("SELECT id, email, nickname, avatar_url, department_id, password_hash, password_salt, phone_code, phone_number, preferred_currency, " +
-        "created_at, deleted_at FROM account WHERE email = #{email}")
+        "created_at, deleted_at FROM account WHERE email = #{email} AND deleted_at IS NULL")
     Account getAccountByEmail(String email);
 
     @Insert("INSERT INTO account (email, password_hash, password_salt) VALUES " +
@@ -26,11 +23,11 @@ public interface AccountDao {
     @Delete("DELETE FROM account WHERE id = #{id}")
     void deleteAccount(int id);
 
-    @Update("UPDATE account SET password_hash = #{passwordHash}, password_salt = #{passwordSalt} WHERE id = #{id}" +
+    @Update("UPDATE account SET password_hash = #{passwordHash}, password_salt = #{passwordSalt} WHERE id = #{id} AND deleted_at IS NULL" +
         "RETURNING id")
     int updatePassword(int id, String passwordHash, String passwordSalt);
 
-    @Update("UPDATE account SET nickname = #{nickname}, avatar_url = #{avatar}, phone_code = #{phoneCode}, phone_number = #{phoneNumber}, preferred_currency = #{currency} WHERE id = #{id}" +
+    @Update("UPDATE account SET nickname = #{nickname}, avatar_url = #{avatar}, phone_code = #{phoneCode}, phone_number = #{phoneNumber}, preferred_currency = #{currency} WHERE id = #{id} AND deleted_at IS NULL" +
         "RETURNING id")
     int updateProfile(String nickname, String avatar, String phoneCode, String phone_number, String currency, int id);
 }
