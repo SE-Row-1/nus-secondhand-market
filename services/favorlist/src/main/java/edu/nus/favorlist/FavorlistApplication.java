1bb16d56ab6754fe273825792ac557ed199e6c1c
MrPhotato <chenzianhhh@gmail.com>
2024-10-06 19:06:58 +0800

    Added directories

---
+++ services/favorlist/src/main/java/edu/nus/favorlist/FavorlistApplication.java
@@ -0,0 +1,13 @@
+package edu.nus.favorlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavorlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavorlistApplication.class, args);
+    }
+
+}
---
+++ services/favorlist/src/test/java/edu/nus/favorlist/FavorlistApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nus.favorlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavorlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
