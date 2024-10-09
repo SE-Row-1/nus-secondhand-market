65b175d9c582ceb7f57f169d8dee252609f70b2f
Yuwang Cai <mrcaidev@gmail.com>
2024-10-09 10:41:53 +0800

    docs: move env docs to .env.example

--- services/web/src/global.d.ts
+++ services/web/src/global.d.ts
@@ -1,24 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      /**
-       * Base URL for browser to access backend API.
-       *
-       * Required.
-       *
-       * @note Do not include a trailing slash.
-       * @example https://nshm.shop
-       */
       NEXT_PUBLIC_API_BASE_URL: string;
-
-      /**
-       * Base URL for web service to access backend API.
-       *
-       * Required.
-       *
-       * @note Do not include a trailing slash.
-       * @example http://service-registry:8761
-       */
       API_BASE_URL: string;
     }
   }
