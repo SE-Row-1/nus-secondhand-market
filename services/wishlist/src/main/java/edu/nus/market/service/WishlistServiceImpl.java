09c10995e6a262b675b49f11239704f0ac89b32a
MrPhotato <chenzianhhh@gmail.com>
2024-10-11 16:30:15 +0800

    Fix: Target directory

--- services/wishlist/src/main/java/edu/nus/market/service/WishlistServiceImpl.java
+++ services/wishlist/src/main/java/edu/nus/market/service/WishlistServiceImpl.java
@@ -2,6 +2,8 @@ package edu.nus.market.service;
 
 import edu.nus.market.dao.WishlistDao;
 import edu.nus.market.pojo.AddLikeReq;
+import edu.nus.market.pojo.ErrorMsg;
+import edu.nus.market.pojo.ErrorMsgEnum;
 import edu.nus.market.pojo.Like;
 import edu.nus.market.converter.ConvertAddLikeReqToLike;
 
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WishlistServiceImpl implements WishlistService {
@@ -26,8 +29,13 @@ public class WishlistServiceImpl implements WishlistService {
     }
 
     @Override
-    public ResponseEntity<Object> addLikeService(AddLikeReq addLikeReq) {
-
-        return ResponseEntity.status(HttpStatus.OK).body(wishlistDao.save(ConvertAddLikeReqToLike.convert(addLikeReq)));
+    public ResponseEntity<Object> addLikeService(AddLikeReq req) {
+        Optional<Like> existingLike = wishlistDao.findByUserIdAndItemId(req.getUserId(), req.getItemId());
+        // Check if the item is already in the wishlist
+        if (existingLike.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorMsg(ErrorMsgEnum.WISHLIST_CONFLICT.ErrorMsg));
+        }
+        wishlistDao.save(ConvertAddLikeReqToLike.convert(req));
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
