0ca9bc5fd09c48f10c425d62635dda67ba72b5b0
Jyf <1254023440@qq.com>
2024-10-15 01:07:12 +0800

    feat: finish ValidationTest

--- services/account/src/main/java/edu/nus/market/pojo/ReqEntity/UpdPswReq.java
+++ services/account/src/main/java/edu/nus/market/pojo/ReqEntity/UpdPswReq.java
@@ -1,6 +1,7 @@
 package edu.nus.market.pojo.ReqEntity;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdPswReq {
     @NotBlank
+    @Size(min = 8, max = 20)
     String oldPassword;
+
     @NotBlank
+    @Size(min = 8, max = 20)
     String newPassword;
 }
