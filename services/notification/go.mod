5214d0e4ac673a85d539f463a49d656dbc2483d0
Yuwang Cai <mrcaidev@gmail.com>
2024-10-15 14:18:54 +0800

    feat: set up notification service (#33)

---
+++ services/notification/go.mod
@@ -0,0 +1,21 @@
+module nshm.shop/notification
+
+go 1.23.2
+
+require (
+	github.com/go-playground/validator/v10 v10.22.1
+	github.com/joho/godotenv v1.5.1
+	github.com/rabbitmq/amqp091-go v1.10.0
+	github.com/resend/resend-go/v2 v2.12.0
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+)
---
+++ services/notification/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"nshm.shop/notification/processors"
+)
+
+func main() {
+	connection, err := amqp.Dial(os.Getenv("RABBITMQ_URI"))
+	if err != nil {
+		log.Panic(err)
+	}
+	defer connection.Close()
+
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer channel.Close()
+
+	err = channel.ExchangeDeclare("notification", "topic", true, false, false, false, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = startQueue[processors.EmailPayload](channel, "email")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Print("service started")
+
+	var forever = make(chan struct{})
+	<-forever
+}
+
+func startQueue[Payload interface{ Process() error }](ch *amqp.Channel, topic string) error {
+	queue, err := ch.QueueDeclare("notification."+topic, true, false, false, false, nil)
+	if err != nil {
+		return fmt.Errorf("failed to declare queue '%s'", queue.Name)
+	}
+
+	err = ch.QueueBind(queue.Name, topic, "notification", false, nil)
+	if err != nil {
+		return fmt.Errorf("failed to bind queue '%s'", queue.Name)
+	}
+
+	deliveries, err := ch.Consume(queue.Name, "", false, false, false, false, nil)
+	if err != nil {
+		return fmt.Errorf("failed to consume queue '%s'", queue.Name)
+	}
+
+	go func() {
+		for delivery := range deliveries {
+			var payload Payload
+
+			err := json.Unmarshal(delivery.Body, &payload)
+			if err != nil {
+				log.Printf("failed to parse payload: %s", err)
+				delivery.Nack(false, false)
+				continue
+			}
+
+			err = payload.Process()
+			if err != nil {
+				log.Printf("failed to process payload: %s", err)
+				delivery.Nack(false, false)
+				continue
+			}
+
+			delivery.Ack(false)
+		}
+	}()
+
+	return nil
+}
