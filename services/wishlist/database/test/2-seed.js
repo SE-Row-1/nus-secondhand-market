5bf59c01fa6e27fc679dd40a50919297fe05eb33
MrPhotato <chenzianhhh@gmail.com>
2024-10-21 16:37:12 +0800

    fix: env used for connection to DB under container environment

--- services/wishlist/database/test/2-seed.js
+++ services/wishlist/database/test/2-seed.js
@@ -1,7 +1,6 @@
 // Insert seed data into 'wishlist' collection
 db.wishlist.insertMany([
   {
-    _id: ObjectId("670f7703ad0e321f3552e02c"),
     _class: "edu.nus.market.pojo.SingleLike",
     itemId: "581cd614-27a1-4716-889d-e9a22fc27f07",
     name: "iPhone 12",
@@ -18,7 +17,6 @@ db.wishlist.insertMany([
     wantedAt: ISODate("2024-10-16T08:19:15.624Z")
   },
   {
-    _id: ObjectId("670f7706ad0e321f3552e02d"),
     _class: "edu.nus.market.pojo.PackLike",
     itemId: "681cd614-27a1-4716-889d-e9a22fc27f07",
     name: "Apple Device Bundle",
@@ -35,7 +33,6 @@ db.wishlist.insertMany([
     discount: 10
   },
   {
-    _id: ObjectId("670f7707ad0e321f3552e02e"),
     _class: "edu.nus.market.pojo.SingleLike",
     itemId: "981cd614-27a1-4716-889d-e9a22fc27f07",
     name: "Samsung Galaxy S21",
@@ -52,7 +49,6 @@ db.wishlist.insertMany([
     wantedAt: ISODate("2024-10-17T10:30:45.120Z")
   },
   {
-    _id: ObjectId("670f7708ad0e321f3552e02f"),
     _class: "edu.nus.market.pojo.PackLike",
     itemId: "781cd614-27a1-4716-889d-e9a22fc27f07",
     name: "Smart Home Bundle",
@@ -69,7 +65,6 @@ db.wishlist.insertMany([
     discount: 15
   },
   {
-    _id: ObjectId("670f7709ad0e321f3552e030"),
     _class: "edu.nus.market.pojo.SingleLike",
     itemId: "881cd614-27a1-4716-889d-e9a22fc27f07",
     name: "PlayStation 5",
@@ -86,7 +81,6 @@ db.wishlist.insertMany([
     wantedAt: ISODate("2024-10-18T09:25:18.743Z")
   },
   {
-    _id: ObjectId("670f7710ad0e321f3552e031"),
     _class: "edu.nus.market.pojo.PackLike",
     itemId: "881cd614-27a1-4716-889d-e9a22fc27f07",
     name: "Gamer Bundle",
