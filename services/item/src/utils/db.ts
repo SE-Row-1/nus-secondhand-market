2e065be01de50e024dca9672966f6f6f0cd3b716
Yuwang Cai <mrcaidev@gmail.com>
2024-10-23 00:12:10 +0800

    docs: db docs

--- services/item/src/utils/db.ts
+++ services/item/src/utils/db.ts
@@ -5,14 +5,12 @@ const client = await MongoClient.connect(Bun.env.MONGO_URL);
 const db = client.db(Bun.env.MONGO_DB_NAME);
 
 /**
- * The MongoDB collection that stores all items.
- *
- * This is the single source of truth for items across all microservices.
+ * Stores all second-hand items.
  */
 export const itemsCollection = db.collection<Item>("items");
 
 /**
- * Host all buyer-seller transactions.
+ * Stores all buyer-seller transactions.
  */
 export const transactionsCollection =
   db.collection<Transaction>("transactions");
