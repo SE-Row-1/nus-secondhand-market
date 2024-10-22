30a290f8f57452fa65d13f0726a3b44c76a8b52e
Yuwang Cai <mrcaidev@gmail.com>
2024-10-22 16:06:56 +0800

    fix: badge hover effect

--- services/web/src/components/item/status-badge.tsx
+++ services/web/src/components/item/status-badge.tsx
@@ -8,10 +8,14 @@ type Props = {
 export function StatusBadge({ status }: Props) {
   switch (status) {
     case ItemStatus.FOR_SALE:
-      return <Badge className="bg-green-600">FOR SALE</Badge>;
+      return (
+        <Badge className="bg-green-600 hover:bg-green-600/80">FOR SALE</Badge>
+      );
     case ItemStatus.DEALT:
-      return <Badge className="bg-yellow-600">DEALT</Badge>;
+      return (
+        <Badge className="bg-yellow-600 hover:bg-yellow-600/80">DEALT</Badge>
+      );
     case ItemStatus.SOLD:
-      return <Badge className="bg-red-600">SOLD</Badge>;
+      return <Badge className="bg-red-600 hover:bg-red-600/80">SOLD</Badge>;
   }
 }
