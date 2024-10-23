323918bca51c813001fab4d8bb8f80bbb18df0a1
Yuwang Cai <mrcaidev@gmail.com>
2024-10-23 08:27:48 +0800

    test: extract mock smms response function

--- services/item/tests/utils/photo-storage-gateway/smms.test.ts
+++ services/item/tests/utils/photo-storage-gateway/smms.test.ts
@@ -2,33 +2,33 @@ import { SmmsPhotoStorageGateway } from "@/utils/photo-storage-gateway/smms";
 import { afterAll, beforeAll, expect, it, mock } from "bun:test";
 import { HTTPException } from "hono/http-exception";
 
-const myFetch = mock();
+const mockFetch = mock();
 
 beforeAll(() => {
-  global.fetch = myFetch;
+  global.fetch = mockFetch;
 });
 
 afterAll(() => {
   mock.restore();
 });
 
+function mockSmmsResponse(status: number, json: unknown) {
+  mockFetch.mockImplementationOnce(
+    async () => new Response(JSON.stringify(json), { status }),
+  );
+}
+
 it("saves and removes photo", async () => {
   const gateway = new SmmsPhotoStorageGateway();
   const photo = new File(["test"], "test.png") as unknown as File;
-  myFetch.mockImplementationOnce(
-    async () =>
-      new Response(
-        JSON.stringify({
-          data: { hash: "abc", url: "https://sm.ms/test.png" },
-        }),
-        { status: 200 },
-      ),
-  );
+  mockSmmsResponse(200, {
+    data: { hash: "abc", url: "https://sm.ms/test.png" },
+  });
 
   const photoUrl = await gateway.save(photo);
 
   expect(photoUrl).toEqual("https://sm.ms/test.png");
-  expect(myFetch).toHaveBeenLastCalledWith("https://sm.ms/api/v2/upload", {
+  expect(mockFetch).toHaveBeenLastCalledWith("https://sm.ms/api/v2/upload", {
     method: "POST",
     body: expect.any(FormData),
     headers: {
@@ -36,58 +36,41 @@ it("saves and removes photo", async () => {
     },
   });
 
-  myFetch.mockImplementationOnce(
-    async () => new Response(null, { status: 200 }),
-  );
+  mockSmmsResponse(200, {});
 
   await gateway.remove(photoUrl);
 
-  expect(myFetch).toHaveBeenLastCalledWith("https://sm.ms/api/v2/delete/abc", {
-    method: "GET",
-    headers: {
-      Authorization: Bun.env.SMMS_API_KEY,
+  expect(mockFetch).toHaveBeenLastCalledWith(
+    "https://sm.ms/api/v2/delete/abc",
+    {
+      method: "GET",
+      headers: {
+        Authorization: Bun.env.SMMS_API_KEY,
+      },
     },
-  });
+  );
 });
 
 it("throws HTTPException if SM.MS upload fails", async () => {
   const gateway = new SmmsPhotoStorageGateway();
   const photo = new File(["test"], "test.png") as unknown as File;
-  myFetch.mockImplementationOnce(
-    async () => new Response(null, { status: 400 }),
-  );
+  mockSmmsResponse(400, {});
 
   const fn = async () => await gateway.save(photo);
 
-  expect(fn).toThrow(
-    new HTTPException(502, {
-      message: 'Failed to upload photo "test.png" to SM.MS',
-    }),
-  );
+  expect(fn).toThrow(HTTPException);
 });
 
 it("throws HTTPException if SM.MS delete fails", async () => {
   const gateway = new SmmsPhotoStorageGateway();
   const photo = new File(["test"], "test.png") as unknown as File;
-  myFetch.mockImplementationOnce(
-    async () =>
-      new Response(
-        JSON.stringify({
-          data: { hash: "abc", url: "https://sm.ms/test.png" },
-        }),
-        { status: 200 },
-      ),
-  );
+  mockSmmsResponse(200, {
+    data: { hash: "abc", url: "https://sm.ms/test.png" },
+  });
   const photoUrl = await gateway.save(photo);
-  myFetch.mockImplementationOnce(
-    async () => new Response(null, { status: 400 }),
-  );
+  mockSmmsResponse(400, {});
 
   const fn = async () => await gateway.remove(photoUrl);
 
-  expect(fn).toThrow(
-    new HTTPException(502, {
-      message: 'Failed to delete photo "https://sm.ms/test.png" from SM.MS',
-    }),
-  );
+  expect(fn).toThrow(HTTPException);
 });
