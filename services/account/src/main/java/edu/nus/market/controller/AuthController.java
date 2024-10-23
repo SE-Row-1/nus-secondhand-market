100218470c0cbb25b8d6100d8716e5d698696f80
Jyf <1254023440@qq.com>
2024-10-24 02:42:49 +0800

    feat: add an extra endpoint for local test temporarily

--- services/account/src/main/java/edu/nus/market/controller/AuthController.java
+++ services/account/src/main/java/edu/nus/market/controller/AuthController.java
@@ -1,10 +1,15 @@
 package edu.nus.market.controller;
 
 import edu.nus.market.dao.AccountDao;
+import edu.nus.market.dao.EmailTransactionDao;
 import edu.nus.market.pojo.ErrorMsg;
 import edu.nus.market.pojo.ErrorMsgEnum;
 import edu.nus.market.pojo.ReqEntity.*;
+import edu.nus.market.pojo.ResEntity.EmailMessage;
+import edu.nus.market.pojo.data.Account;
+import edu.nus.market.pojo.data.EmailTransaction;
 import edu.nus.market.security.JwtTokenManager;
+import edu.nus.market.security.OTPGenerator;
 import edu.nus.market.service.AccountService;
 import edu.nus.market.service.EmailValidationService;
 import jakarta.annotation.Resource;
@@ -13,6 +18,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/auth")
@@ -27,6 +34,12 @@ public class AuthController {
 
     @Resource
     EmailValidationService emailValidationService;
+
+    @Resource
+    EmailTransactionDao emailTransactionDao;
+
+    @Resource
+    OTPGenerator otpGenerator;
 
     // Get Account Info
     @GetMapping("/me")
@@ -84,6 +97,38 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMsg(ErrorMsgEnum.INVALID_DATA_FORMAT.ErrorMsg));
         }
         return emailValidationService.sendOTP(emailOTPReq);
+    }
+
+    // use this to help test sending opt
+    @PostMapping("/otp/test")
+    public ResponseEntity<Object> sendOtpTest(@Valid @RequestBody EmailOTPReq emailOTPReq, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMsg(ErrorMsgEnum.INVALID_DATA_FORMAT.ErrorMsg));
+        }
+        String email = emailOTPReq.getEmail();
+        String type = emailOTPReq.getType();
+        //
+        Account account = accountDao.getAccountByEmail(email);
+        // for register and update_email, check if the email is not registered
+        if (type.equals("register") || type.equals("update_email")){
+            if (account != null)
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorMsg(ErrorMsgEnum.REGISTERED_EMAIL.ErrorMsg));
+        }
+        // for reset password, check if the email exists
+        else if (type.equals("reset_password")){
+            if (account == null)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMsg(ErrorMsgEnum.ACCOUNT_NOT_FOUND.ErrorMsg));
+        }
+        else return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMsg(ErrorMsgEnum.INVALID_DATA_FORMAT.ErrorMsg));
+
+        String otp = otpGenerator.generateOTP(6);
+        EmailMessage message = new EmailMessage(email, "Your Verification Code", "Your verification code is: " + otp);
+
+
+
+        UUID id = emailTransactionDao.insertEmailTransaction(new EmailTransaction(email, otp));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
 
     @PostMapping("/otp/verification")
