57ae8440682f42f6bf757c04eed1ba581b22d556
Yuwang Cai <mrcaidev@gmail.com>
2024-10-24 21:16:57 +0800

    fix: restore requester mock in get-one-item test

--- services/item/tests/items/get-one.test.ts
+++ services/item/tests/items/get-one.test.ts
@@ -1,12 +1,18 @@
 import { ItemStatus, ItemType } from "@/types";
 import { itemsCollection } from "@/utils/db";
-import { expect, it, mock } from "bun:test";
+import { afterAll, beforeAll, expect, it, mock } from "bun:test";
 import { me } from "../test-utils/mock-data";
 import { GET } from "../test-utils/request";
 
-mock.module("@/utils/requester", () => ({
-  createRequester: () => () => me.detailedAccount,
-}));
+beforeAll(() => {
+  mock.module("@/utils/requester", () => ({
+    createRequester: () => () => me.detailedAccount,
+  }));
+});
+
+afterAll(() => {
+  mock.restore();
+});
 
 it("returns the item with the given ID", async () => {
   const insertedId = crypto.randomUUID();
