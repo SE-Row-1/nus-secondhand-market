2d98715ca7e51fe830e5f524bfb46096591feca4
Yuwang Cai <mrcaidev@gmail.com>
2024-10-24 19:39:01 +0800

    feat: react query dev tools

--- services/web/src/query/provider.tsx
+++ services/web/src/query/provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { PropsWithChildren } from "react";
 import { getQueryClient } from "./client";
 
@@ -8,6 +9,7 @@ export function QueryProvider({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={getQueryClient()}>
       {children}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
