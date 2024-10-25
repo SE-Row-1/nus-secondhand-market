fd7aaffdf114541c5bcd9b8997855fa05a5f0b60
amber <>
2024-10-25 14:45:55 +0800

    test: add test file to trigger build image of web

--- services/account/src/main/java/edu/nus/market/config/GlobalCors.java
+++ services/account/src/main/java/edu/nus/market/config/GlobalCors.java
@@ -1,21 +1,22 @@
-package edu.nus.market.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class GlobalCors implements WebMvcConfigurer {
-    @Value("${spring.availableDomain.frontendUrl}")
-    String EC2_PUBLIC_IP;
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins(EC2_PUBLIC_IP, "http://localhost") // 允许所有域
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-    }
-}
-
+//package edu.nus.market.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class GlobalCors implements WebMvcConfigurer {
+//    @Value("${spring.availableDomain.frontendUrl}")
+//    String EC2_PUBLIC_IP;
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins(EC2_PUBLIC_IP, "http://localhost") // 允许所有域
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
+//            .allowedHeaders("*")
+//            .allowCredentials(true);
+//    }
+//}
+//
+//
--- services/item/database/production/init.js
+++ services/item/database/production/init.js
@@ -1,23 +1,7 @@
 /* eslint-disable no-undef */
 
-db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
-
 db.createCollection("items");
 
 db.items.createIndex({ name: "text", description: "text" });
-
-if (db.getUser(process.env.MONGO_INITDB_ROOT_USERNAME) === null) {
-  db.createUser({
-    user: process.env.MONGO_INITDB_ROOT_USERNAME,
-    pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
-    roles: [
-      {
-        role: "readWrite",
-        db: process.env.MONGO_INITDB_DATABASE
-      }
-    ]
-  });
-  print("User created: " + process.env.MONGO_INITDB_ROOT_USERNAME);
-}
 
 db.createCollection("transactions");
