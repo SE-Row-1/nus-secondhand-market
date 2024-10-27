b8c144c8d859f40356824c2bd0f47ff67c34c372
Jyf <1254023440@qq.com>
2024-10-27 16:00:53 +0800

    refactor: change the init files structure and

---
+++ services/account/database/test/2-seed.sql
@@ -0,0 +1,12 @@
+insert into department (acronym, name) values
+('ISS', 'Institute of Systems Science'),
+('SOC', 'School of Computing'),
+('BIZ', 'Business School'),
+('ECE', 'Electrical and Computer Engineering');
+
+  insert into account (email, password_hash, password_salt, nickname, avatar_url) values
+  ('e1234561@u.nus.edu', '$2a$10$/UwR7JKUFzyPfLLs9.DlyOXtWMLgPEob0jfF0I6sEK85t7z0vNLlK', '/+uNyqPxTo0OAecGgo3Oog==', 'Johnny', 'https://avatars.githubusercontent.com/u/78269445?v=4'),
+  ('e1234562@u.nus.edu', '$2a$10$j4WHKfF8BRc3Vd5riEZ/8OrnU6dXIIx3vT/qf9y38lRDWy764lYQG', 'Vq3iUtK70eav3Rd0VjYFog==', 'JaneS', 'https://avatars.githubusercontent.com/u/69978374?v=4'),
+  ('e1234563@u.nus.edu', '$2a$10$.QM6HzIPoJOllcbOdDhMxOSM6eGPs9ACV08WA.eBMEjuVAnObsqUi', 'ypJaYX91LwNjtmjjf4H8YQ==', 'AlexL', 'https://avatars.githubusercontent.com/u/13389461?v=4'),
+  ('e1234564@u.nus.edu', '$2a$10$04CYtCiQ/.tP4wTz5rmH.ev9t2wYxfjOHx64Cp2YiaDidiIPj9MXa', 'taHXrSQ0f9HJeT/N5OgeuQ==', 'MikeB', 'https://avatars.githubusercontent.com/u/60336739?v=4'),
+  ('e1234565@u.nus.edu', '$2a$10$OeA4WtLAccv12uylxYnqneVcAGsj0IgSerUGPXX41vRGKRlGLpf3K', 'pix8BBby+8fpK7NnUT07iA==', 'EmJ', 'https://avatars.githubusercontent.com/u/83934144?v=4');
