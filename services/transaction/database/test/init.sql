5a53e1c8085cb943e580af96fe6c3e306a8a7311
Yuwang Cai <mrcaidev@gmail.com>
2024-10-29 23:08:38 +0800

    chore: database init file

--- services/transaction/database/test/init.sql
+++ services/transaction/database/test/init.sql
@@ -3,19 +3,19 @@ create table transaction (
   item_id uuid not null,
   item_name text not null,
   item_price numeric not null,
-  buyer_id integer not null,
-  buyer_nickname text,
-  buyer_avatar_url text,
   seller_id integer not null,
   seller_nickname text,
   seller_avatar_url text,
+  buyer_id integer not null,
+  buyer_nickname text,
+  buyer_avatar_url text,
   created_at timestamptz default now() not null,
   completed_at timestamptz default null,
   cancelled_at timestamptz default null
 );
 
-insert into transaction (item_id, item_name, item_price, buyer_id, buyer_nickname, buyer_avatar_url, seller_id, seller_nickname, seller_avatar_url, completed_at, cancelled_at) values
-('10f33906-24df-449d-b4fb-fcc6c76606b6', 'test', 10, 1, 'me', 'https://example.com/me.jpg', 2, 'someone', 'https://example.com/someone.jpg', null, null),
-('0e80d1ea-142d-4456-af29-34c4628bace3', 'test', 10, 1, 'me', 'https://example.com/me.jpg', 2, 'someone', 'https://example.com/someone.jpg', null, now()),
-('95092868-e35d-46b9-a873-e44cb938e674', 'test', 10, 2, 'someone', 'https://example.com/someone.jpg', 1, 'me', 'https://example.com/me.jpg', now(), null),
-('4515e8cd-2a8b-440a-9e3c-8a57b80724d1', 'test', 10, 2, 'someone', 'https://example.com/someone.jpg', 3, 'foo', 'https://example.com/foo.jpg', null, null);
+insert into transaction (item_id, item_name, item_price, seller_id, seller_nickname, seller_avatar_url, buyer_id, buyer_nickname, buyer_avatar_url, completed_at, cancelled_at) values
+('10f33906-24df-449d-b4fb-fcc6c76606b6', 'item1', 10, 1, 'nickname1', 'https://example.com/avatar1.jpg', 2, 'nickname2', 'https://example.com/avatar2.jpg', null, now()),
+('10f33906-24df-449d-b4fb-fcc6c76606b6', 'item1', 10, 1, 'nickname1', 'https://example.com/avatar1.jpg', 3, 'nickname3', 'https://example.com/avatar3.jpg', now(), null),
+('2475bad8-43ca-4d23-99ee-224d7d9e6ba7', 'item2', 20, 1, 'nickname1', 'https://example.com/avatar1.jpg', 2, 'nickname2', 'https://example.com/avatar2.jpg', null, null),
+('3aed59b0-0fa7-4b8b-b1fd-39b8f46a75b3', 'item3', 30, 1, 'nickname1', 'https://example.com/avatar1.jpg', 3, 'nickname3', 'https://example.com/avatar3.jpg', null, null);
