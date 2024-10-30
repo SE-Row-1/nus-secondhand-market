be10ee9134c63f3bb6f557bc076a9b41c9ea58c3
Yuwang Cai <mrcaidev@gmail.com>
2024-10-30 13:21:38 +0800

    test: requester refactor

--- services/transaction/tests/utils/requester.unit.test.ts
+++ services/transaction/tests/utils/requester.unit.test.ts
@@ -1,62 +1,73 @@
 import { createRequester } from "@/utils/requester";
-import { afterEach, beforeEach, expect, it, mock } from "bun:test";
+import { afterAll, afterEach, expect, it, mock, spyOn } from "bun:test";
 import { HTTPException } from "hono/http-exception";
 
-const mockFetch = mock();
-
-beforeEach(() => {
-  global.fetch = mockFetch;
-});
+const mockFetch = spyOn(global, "fetch");
 
 afterEach(() => {
+  mockFetch.mockClear();
+});
+
+afterAll(() => {
   mock.restore();
 });
 
-it("concatenates endpoint with the right base URL", async () => {
-  mockFetch.mockResolvedValueOnce(
+it("concatenates endpoint with correct base URL", async () => {
+  mockFetch.mockResolvedValue(
     new Response(JSON.stringify({}), { status: 200 }),
   );
 
-  await createRequester("account")("/accounts/1");
+  await createRequester("account")("/");
 
   expect(mockFetch).toHaveBeenLastCalledWith(
-    Bun.env.ACCOUNT_SERVICE_BASE_URL + "/accounts/1",
+    Bun.env.ACCOUNT_SERVICE_BASE_URL + "/",
+    {},
+  );
+
+  await createRequester("item")("/");
+
+  expect(mockFetch).toHaveBeenLastCalledWith(
+    Bun.env.ITEM_SERVICE_BASE_URL + "/",
     {},
   );
 });
 
-it("returns camel-case JSON response", async () => {
-  mockFetch.mockResolvedValueOnce(
-    new Response(JSON.stringify({ foo_bar: [0] }), { status: 200 }),
+it("returns JSON response in camel case", async () => {
+  mockFetch.mockResolvedValue(
+    new Response(JSON.stringify({ foo_bar: [{ bar_baz: 0 }] }), {
+      status: 200,
+    }),
   );
 
   const res = await createRequester("account")("/");
 
-  expect(res).toEqual({ data: { fooBar: [0] }, error: null });
+  expect(res).toEqual({ data: { fooBar: [{ barBaz: 0 }] }, error: null });
 });
 
 it("returns undefined if status is 204", async () => {
-  mockFetch.mockResolvedValueOnce(new Response(null, { status: 204 }));
+  mockFetch.mockResolvedValue(new Response(null, { status: 204 }));
 
   const res = await createRequester("account")("/");
 
   expect(res).toEqual({ data: undefined, error: null });
 });
 
-it("returns HTTPException if not ok", async () => {
-  mockFetch.mockResolvedValueOnce(
+it("returns HTTPException with upstream status code if not ok", async () => {
+  mockFetch.mockResolvedValue(
     new Response(JSON.stringify({ error: "test" }), { status: 400 }),
   );
 
   const res = await createRequester("account")("/");
 
   expect(res).toEqual({ data: null, error: expect.any(HTTPException) });
+  expect(res.error?.status).toEqual(400);
 });
 
-it("returns HTTPException if fetch fails", async () => {
-  mockFetch.mockRejectedValueOnce("test");
+it("returns HTTPException 500 if fetch fails", async () => {
+  mockFetch.mockRejectedValue(new Error("test"));
 
   const res = await createRequester("account")("/");
 
   expect(res).toEqual({ data: null, error: expect.any(HTTPException) });
+  expect(res.error?.status).toEqual(500);
 });
