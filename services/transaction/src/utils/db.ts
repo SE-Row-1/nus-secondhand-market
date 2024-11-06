8882d5e37a5d5ec1963a7a7f3370fb90b5e55add
Yuwang Cai <mrcaidev@gmail.com>
2024-11-06 14:47:27 +0800

    fix: possible fix pg connection issue in ci

--- services/transaction/src/utils/db.ts
+++ services/transaction/src/utils/db.ts
@@ -1,5 +1,11 @@
 import { Pool, types } from "pg";
 
-export const db = new Pool();
+export const db = new Pool({
+  host: Bun.env.PGHOST,
+  port: parseInt(Bun.env.PGPORT, 10),
+  user: Bun.env.PGUSER,
+  password: Bun.env.PGPASSWORD,
+  database: Bun.env.PGDATABASE,
+});
 
 types.setTypeParser(types.builtins.NUMERIC, (value) => parseInt(value, 10));
