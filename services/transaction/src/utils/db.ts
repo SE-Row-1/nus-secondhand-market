bf8f3c421b2f8701132d5d301fa9a9345ef099b4
Yuwang Cai <mrcaidev@gmail.com>
2024-11-06 15:00:42 +0800

    chore: remove redundant variables

--- services/transaction/src/utils/db.ts
+++ services/transaction/src/utils/db.ts
@@ -1,11 +1,5 @@
 import { Pool, types } from "pg";
 
-export const db = new Pool({
-  host: Bun.env.PGHOST,
-  port: parseInt(Bun.env.PGPORT, 10),
-  user: Bun.env.PGUSER,
-  password: Bun.env.PGPASSWORD,
-  database: Bun.env.PGDATABASE,
-});
+export const db = new Pool();
 
 types.setTypeParser(types.builtins.NUMERIC, (value) => parseInt(value, 10));
