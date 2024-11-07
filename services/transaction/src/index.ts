47ce2122fb5b4596eaf639cfac83273a65ebfc1a
Yuwang Cai <mrcaidev@gmail.com>
2024-11-07 11:23:50 +0800

    chore: no secure headers

--- services/transaction/src/index.ts
+++ services/transaction/src/index.ts
@@ -1,7 +1,6 @@
 import "@/events/consume";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import { secureHeaders } from "hono/secure-headers";
 import { globalErrorHandler } from "./middleware/global-error-handler";
 import { globalNotFoundHandler } from "./middleware/global-not-found-handler";
 import { transformCase } from "./middleware/transform-case";
@@ -11,7 +10,7 @@ import { transactionsController } from "./transactions/controller";
 const app = new Hono();
 
 // Global middleware.
-app.use(logger(), secureHeaders(), transformCase());
+app.use(logger(), transformCase());
 
 // Probes.
 app.get("/healthz", (c) => c.text("ok"));
