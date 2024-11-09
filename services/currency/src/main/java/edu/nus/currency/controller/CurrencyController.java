fe2f53c38a4147fe991c5b3a13d03f6b873d0150
Jyf <1254023440@qq.com>
2024-11-09 21:37:09 +0800

    refactor: refactor currency construction

--- services/currency/src/main/java/edu/nus/currency/controller/CurrencyController.java
+++ services/currency/src/main/java/edu/nus/currency/controller/CurrencyController.java
@@ -4,10 +4,12 @@ import edu.nus.currency.pojo.ErrorMsg;
 import edu.nus.currency.pojo.ErrorMsgEnum;
 import edu.nus.currency.pojo.ResEntity.JWTPayload;
 import edu.nus.currency.pojo.ReqEntity.UpdExgRatReq;
+import edu.nus.currency.pojo.UpdateCurrencyMessage;
 import edu.nus.currency.security.JwtTokenManager;
 import edu.nus.currency.service.ExchangeRateService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -19,6 +21,12 @@ import org.springframework.web.bind.annotation.*;
 public class CurrencyController {
     @Resource
     ExchangeRateService exchangeRateService;
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public CurrencyController(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     @PutMapping ("")
     public ResponseEntity<Object> updateCurrencies(@Valid @RequestBody UpdExgRatReq updExgRatReq, BindingResult bindingResult){
@@ -35,4 +43,19 @@ public class CurrencyController {
         return exchangeRateService.getPreferredCurrencyService(preferredCurrency);
     }
 
+    @PostMapping ("")
+    public ResponseEntity<Object> addSubscription(@RequestBody UpdateCurrencyMessage updateCurrencyMessage){
+        String key = "subscription";
+        redisTemplate.opsForHash().increment(key, updateCurrencyMessage.getNewCurrency(), 1); // Increment the subscriber count by 1.increment(key, 1); // Track how many times a user subscribed
+        if (updateCurrencyMessage.getOldCurrency() != null && updateCurrencyMessage.getOldCurrency() != ""){
+            redisTemplate.opsForHash().increment(key, updateCurrencyMessage.getOldCurrency(), -1);
+            if (Integer.parseInt(redisTemplate.opsForHash().get(key, updateCurrencyMessage.getOldCurrency()).toString()) == 0){
+                redisTemplate.opsForHash().delete(key, updateCurrencyMessage.getOldCurrency());
+            }
+        }
+
+        redisTemplate.persist(key); // Persist the subscription hash to disk
+
+        return ResponseEntity.ok().build();
+    }
 }
--- services/currencyBackup/src/main/java/edu/nus/market/service/ExchangeRateService.java
+++
@@ -1,9 +0,0 @@
-package edu.nus.market.service;
-
-import edu.nus.market.pojo.UpdExgRatReq;
-import org.springframework.http.ResponseEntity;
-
-public interface ExchangeRateService {
-    ResponseEntity<Object> updateExchangeRate(UpdExgRatReq req);
-
-}
