87e5e21b90e7272bfcaee3eef12f8a5972732b38
MrPhotato <137085785+MrPhotato@users.noreply.github.com>
2024-11-10 12:42:22 +0800

    fix: change Wishlist Queue Name and add logs (#68)
    Co-authored-by: amber <>
    Co-authored-by: nshm <nshm@u.nus.edu>

---
+++ services/wishlist/src/main/java/edu/nus/market/service/RabbitMQConnectionListener.java
@@ -0,0 +1,23 @@
+package edu.nus.market.service;
+
+import org.springframework.amqp.rabbit.connection.Connection;
+import org.springframework.amqp.rabbit.connection.ConnectionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConnectionListener implements ConnectionListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQConnectionListener.class);
+
+    @Override
+    public void onCreate(Connection connection) {
+        logger.info("Successfully connected to RabbitMQ.");
+    }
+
+    @Override
+    public void onClose(Connection connection) {
+        logger.warn("Disconnected from RabbitMQ.");
+    }
+}
